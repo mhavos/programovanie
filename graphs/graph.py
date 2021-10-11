0701193f50bb2165eb67a7674d0f91e26ce4482c
mhavos <matej.haverlik@gmail.com>
2021-10-11 14:02:37 +0200

    2021-10-11 Dijkstra

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,3 +1,15 @@
+class Container:
+    def __init__(self, val, data, prev=None):
+        self.val = val
+        self.data = data
+        self.prev = prev
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __le__(self, other):
+        return self.val <= other.val
+
 class Graph:
     r = 20
 
@@ -39,13 +51,49 @@ class Graph:
             for v2 in v1.neighbors:
                 if v2.id < v1.id:
                     continue
-                canvas.create_line(v1.x, v1.y, v2.x, v2.y)
-                lx, ly = (v1.x + v2.x)/2, (v1.y + v2.y)/2
-                canvas.create_rectangle(lx - 20, ly - 10, lx + 20, ly + 10, fill="white", outline="white")
-                canvas.create_text(lx, ly, text=str(v1.neighbors[v2]))
-            canvas.create_oval(v1.x - Graph.r, v1.y - Graph.r, v1.x + Graph.r, v1.y + Graph.r, fill=v1.color)
-            canvas.create_text(v1.x, v1.y, text=str(v1.id))
+                self.create(canvas, v1, v2)
+            self.create(canvas, v1)
 
     def remove(self, vertex):
         vertex.remove()
         self.vertices.remove(vertex)
+
+    def dijkstra(self, v1, v2, canvas):
+        for vertex in self.vertices:
+            vertex.visited = False
+
+        import heapq
+        h = [Container(0, v1)]
+        while h:
+            next = heapq.heappop(h)
+            v = next.data
+            if v.visited:
+                continue
+            v.visited = True
+            v.prev = next.prev
+            if v is v2:
+                return self.trace(canvas, v2)
+            for n in v.neighbors:
+                heapq.heappush(h, Container(next.val + v.neighbors[n], n, prev = v))
+
+        return False
+
+    def trace(self, canvas, v2):
+        self.draw(canvas)
+        current = v2
+        while current.prev != None:
+            self.create(canvas, current, current.prev, thick=1)
+            self.create(canvas, current, thick=1)
+            current = current.prev
+        self.create(canvas, current, thick=1)
+
+    def create(self, canvas, v1, v2=None, thick=0):
+        width = 1 + 2*thick
+        if v2 is not None:
+            canvas.create_line(v1.x, v1.y, v2.x, v2.y, width=width)
+            lx, ly = (v1.x + v2.x)/2, (v1.y + v2.y)/2
+            canvas.create_rectangle(lx - 20, ly - 10, lx + 20, ly + 10, fill="white", outline="white")
+            canvas.create_text(lx, ly, text=str(v1.neighbors[v2]))
+        else:
+            canvas.create_oval(v1.x - Graph.r, v1.y - Graph.r, v1.x + Graph.r, v1.y + Graph.r, fill=v1.color, width=width)
+            canvas.create_text(v1.x, v1.y, text=str(v1.id))
--- graphs/main.py
+++ graphs/main.py
@@ -64,6 +64,70 @@ def remove(event):
         marked = None
     graph.draw(canvas)
 
+last = None
+def middle(event):
+    clicked = None
+    for vertex in graph.vertices:
+        if (event.x - vertex.x)**2 + (event.y - vertex.y)**2 <= Graph.r**2:
+            clicked = vertex
+            break
+    if clicked is None:
+        return
+
+    global last
+    if last is clicked:
+        last = None
+        graph.draw(canvas)
+        return
+    if last is None:
+        last = clicked
+    graph.dijkstra(last, clicked, canvas)
+    last = clicked
+
+ds = None
+def dijkstrastart(event):
+    clicked = None
+    for vertex in graph.vertices:
+        if (event.x - vertex.x)**2 + (event.y - vertex.y)**2 <= Graph.r**2:
+            clicked = vertex
+            break
+    if clicked is None:
+        return
+
+    global ds
+    ds = clicked
+    dijkstralol(event)
+
+def dijkstraend(event):
+    global ds
+
+    clicked = None
+    for vertex in graph.vertices:
+        if (event.x - vertex.x)**2 + (event.y - vertex.y)**2 <= Graph.r**2:
+            clicked = vertex
+            break
+    if clicked is None:
+        ds = None
+    if ds is None:
+        graph.draw(canvas)
+        return
+    a = graph.dijkstra(ds, clicked, canvas)
+    if a == False:
+        ds = None
+        graph.draw(canvas)
+
+def dijkstralol(event):
+    global ds
+    if ds is None:
+        return
+    graph.draw(canvas)
+    canvas.create_line(ds.x, ds.y, event.x, event.y, width=3)
+    graph.create(canvas, ds, thick=1)
+
+
 canvas.bind("<Button-1>", handler)
+canvas.bind("<ButtonPress-2>", dijkstrastart)
+canvas.bind("<B2-Motion>", dijkstralol)
+canvas.bind("<ButtonRelease-2>", dijkstraend)
 canvas.bind("<Button-3>", remove)
 window.mainloop()
